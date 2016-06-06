2e7f43e2b67e19cecbd588c010c756e3b39d1da8
stefan <ssavu@jahia.com>
2016-06-06 13:16:51 -0400

    MOD-974 Created service that will perform request to retrieve connection status based on a fixed interval.

---
+++ src/main/javascript/components/main/connectionStatus/connectionStatusService.js
@@ -0,0 +1,41 @@
+(function(){
+    var connectionStatusService = function(contextualData, dcDataFactory, $rootScope, $interval) {
+
+        var currentConnectionStatus = null;
+        var connectionStatusInterval = null;
+        var REQUEST_TIMEOUT = 1000;
+        var connectionId;
+        var databaseType;
+
+        this.enable = function(id, dbType) {
+            connectionId = id;
+            databaseType = dbType;
+            connectionStatusInterval = $interval(requestConnectionStatusEvent, REQUEST_TIMEOUT);
+        };
+
+        this.disable = function() {
+            if (connectionStatusInterval !== null) {
+                $interval.cancel(connectionStatusInterval);
+            }
+        };
+
+        this.getCurrentConnectionStatus = function() {
+            return currentConnectionStatus;
+        };
+
+        function requestConnectionStatusEvent() {
+           dcDataFactory.customRequest({
+                url: contextualData.context + '/modules/databaseconnector/' + contextualData.entryPoints[databaseType] + '/status/' + connectionId, 
+                method: 'GET'
+           }).then(function(response) {
+               currentConnectionStatus = response;
+               $rootScope.$broadcast('connectionStatusUpdate', currentConnectionStatus);
+           }, function() {
+           });
+        }
+    };
+
+    angular
+        .module('databaseConnector')
+        .service('dcConnectionStatusService', ['contextualData', 'dcDataFactory', '$rootScope', '$interval', connectionStatusService]);
+})();
