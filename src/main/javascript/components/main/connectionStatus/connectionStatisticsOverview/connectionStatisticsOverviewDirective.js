cf1c2c1b1c4a433ded879afe28cedc140371080c
stefan <ssavu@jahia.com>
2016-06-14 10:55:56 -0400

    Code refactor - Removed unused information.

--- src/main/javascript/components/main/connectionStatus/connectionStatisticsOverview/connectionStatisticsOverviewDirective.js
+++ src/main/javascript/components/main/connectionStatus/connectionStatisticsOverview/connectionStatisticsOverviewDirective.js
@@ -25,7 +25,6 @@
         
         var cso = this;
         cso.goToConnections = goToConnections;
-        cso.getTimeFromNow = getTimeFromNow;
         init();
 
         function init() {
@@ -34,13 +33,6 @@
             $scope.$on('connectionStatusUpdate', function(event, connectionStatus) {
                 cso.connectionStatus = connectionStatus;
             });
-        }
-
-        function getTimeFromNow() {
-            if (cso.connectionStatus === null) {
-                return null;
-            }
-            return moment(cso.connectionStatus.uptime.unix).fromNow().toString();
         }
         
         function goToConnections() {
