c8e70564e4063de0e79b84346f56f0d9c865dbd4
cedmail <cmailleux@jahia.com>
2016-08-09 14:20:11 -0400

    First code review, little fixes

--- src/main/javascript/components/main/databaseConnectorModule.js
+++ src/main/javascript/components/main/databaseConnectorModule.js
@@ -1,15 +1,15 @@
-(function(){
+(function () {
     'use strict';
 
     angular.module('databaseConnector', [
         'ui.router', 'datatables', 'datatables.bootstrap', 'i18n',
         'ui.bootstrap', 'ngResource', 'databaseConnector.downloadFactory',
-        'checklist-model', 'uiSwitch','toaster', 'ngAnimate', 'ngSanitize', 'ngMaterial', 'ngMessages',
+        'checklist-model', 'uiSwitch', 'toaster', 'ngAnimate', 'ngSanitize', 'ngMaterial', 'ngMessages',
         'databaseConnector.dataFactory', 'ngFileUpload', 'googlechart'
     ]).config(function ($mdThemingProvider) {
         $mdThemingProvider.theme('blue-theme', 'default')
             .primaryPalette('light-blue',
-                {'default': 'A400'});
+                {'default': '300'});
         
     });
 })();
