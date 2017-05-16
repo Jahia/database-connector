d079626a6b4ea83d4b299ce0801428c0c13b0844
akarmanov <alexandrk_@hotmail.com>
2017-05-16 11:22:52 -0400

    MOD-1715 Remove context from constructor

--- src/main/java/org/jahia/modules/databaseConnector/api/DCApiApplication.java
+++ src/main/java/org/jahia/modules/databaseConnector/api/DCApiApplication.java
@@ -5,15 +5,13 @@ import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.jahia.modules.databaseConnector.api.filters.HeadersResponseFilter;
-import org.jahia.modules.databaseConnector.util.Utils;
-import org.osgi.framework.BundleContext;
 
 /**
  * @author alexander karmanov on 2017-05-16.
  */
 public class DCApiApplication extends ResourceConfig {
 
-    public DCApiApplication(BundleContext context) {
+    public DCApiApplication() {
         super(
                 DCApi.class,
                 JacksonJaxbJsonProvider.class,
