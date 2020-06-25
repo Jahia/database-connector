9b84f970860507f3119b819cb4d3e2a48fdc0690
Vladislav <vladislav@vradan-macbook.local>
2020-06-25 11:49:53 -0400

    test

--- tests/buildEnv/docker-startup.sh
+++ tests/buildEnv/docker-startup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-START_TIME=$SECONDS
 MANIFEST=$1
 
 if [[ $MANIFEST == "" ]]; then
@@ -10,8 +9,7 @@ fi
 echo " == Waiting for Jahia to startup http://${JAHIA_HOST}:${JAHIA_PORT}"
 jahia-cli alive --jahiaAdminUrl=http://${JAHIA_HOST}:${JAHIA_PORT}
 
-ELAPSED_TIME=$(($SECONDS - $START_TIME))
-echo " == Jahia became alive in ${ELAPSED_TIME} seconds"
+echo "Jahia is alive"
 
 # Execute jobs listed in the manifest
 sed -i -e "s/NEXUS_USERNAME/${NEXUS_USERNAME}/g" /tmp/buildEnv/${MANIFEST}
@@ -19,10 +17,9 @@ sed -i -e "s/NEXUS_PASSWORD/${NEXUS_PASSWORD}/g" /tmp/buildEnv/${MANIFEST}
 echo "Download artifacts from Nexus"
 echo "/tmp/buildEnv/${MANIFEST}"
 jahia-cli manifest:run --manifest=/tmp/buildEnv/${MANIFEST} --jahiaAdminUrl=http://${JAHIA_HOST}:${JAHIA_PORT}
-END_BUILD=$(($SECONDS - $INT_TIME))
-echo " == Jahia was updated in ${END_BUILD} seconds"
+echo "Jahia was updated"
 sleep 5
 
 echo "Run cypress tests"
-yarn cypress run
+yarn cypress run --headless --browser chrome
 echo "Tests Done"
