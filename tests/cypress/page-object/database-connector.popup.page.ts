1a921ed29c2c15d664894396423c20e83f9fe5d4
Vladislav <vladislav@vradan-macbook.local>
2020-06-25 15:08:51 -0400

    test

--- tests/cypress/page-object/database-connector.popup.page.ts
+++ tests/cypress/page-object/database-connector.popup.page.ts
@@ -19,7 +19,8 @@ class DatabaseConnectorPopupPage extends DatabaseConnectorBasePage {
         // this.checkbox(this.elements.enableConnection, true)
         super.getElementInIframe(this.elements.host).type(host)
         super.getElementInIframe(this.elements.port).clear().type(port)
-        super.getElementInIframe(this.elements.id).type(id).should('contain', id)
+        super.getElementInIframe(this.elements.id).type(id)
+        cy.wait(1000)
         super.getElementInIframe(this.elements.createButton).click()
         return this
     }
