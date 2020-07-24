6904ab4315531f72e020c4d68440c5b009fcebc0
ksagnard@jahia.com <ksagnard@jahia.com>
2020-07-24 16:51:07 +0200

    merge with signature

--- tests/cypress/page-object/database-connector.page.ts
+++ tests/cypress/page-object/database-connector.page.ts
@@ -1,9 +1,14 @@
 import { DatabaseConnectorBasePage } from './database-connector.base.page'
 import { databaseConnectorPopup } from './database-connector.popup.page'
+import { deleteConnectorPopup } from './delete-connector.popup.page'
 
 class DatabaseConnectorPage extends DatabaseConnectorBasePage {
     elements = {
         newConnection: '.connection-containerMain button',
+        connection3Dots: "[ng-click*='open']",
+        menuContainer: "[id*='menu_container'][aria-hidden='false']",
+        editConnection: "[ng-click*='editConnection']",
+        deleteConnection: "[ng-click*='openDeleteConnection']",
         databaseType: "[src*='elasticsearch']",
         connectionId: "[message-key*='connectionName']", // Use parent with this element
         host: "[message-key*='host']", // Use parent with this element
@@ -19,11 +24,35 @@ class DatabaseConnectorPage extends DatabaseConnectorBasePage {
         return databaseConnectorPopup
     }
 
+    clickOnEditConnection(connectionId: string) {
+        super.getByText('md-card', connectionId).find('i').click()
+        cy.get(this.elements.menuContainer).find(this.elements.editConnection).click()
+        return databaseConnectorPopup
+    }
+
+    clickOnDeleteConnection(connectionId: string) {
+        this.getByText('md-card', connectionId).find('i').click()
+        this.getElementInIframe(this.elements.menuContainer).find(this.elements.deleteConnection).click()
+        return deleteConnectorPopup
+    }
+
     verifyConnectionExists(host: string, port: string, id: string) {
         cy.get(this.elements.connectionId).parent().should('contain', id)
         cy.get(this.elements.host).parent().should('contain', host)
         cy.get(this.elements.port).parent().should('contain', port)
     }
+
+    createConnection(host: string, port: string, id: string) {
+        cy.request('POST', 'modules/dbconn/elasticsearch7/add', {
+            port: port,
+            isConnected: true,
+            host: host,
+            id: id,
+            user: '',
+            password: '',
+        })
+    }
+
     /**
      * delete all connections
      */
