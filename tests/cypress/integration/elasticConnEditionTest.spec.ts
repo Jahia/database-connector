cdfe0f70c8a38b2590746a9edbfa89815c142908
Vladislav Radan <vradan@jahia.com>
2020-08-18 12:05:47 -0400

    minor

--- tests/cypress/integration/elasticConnEditionTest.spec.ts
+++ tests/cypress/integration/elasticConnEditionTest.spec.ts
@@ -1,37 +1,37 @@
-// import { databaseConnector } from '../page-object/database-connector.page'
+import { databaseConnector } from '../page-object/database-connector.page'
 
-// describe('elastic search test', () => {
-//     let connId: string
+describe('elastic search test', () => {
+    let connId: string
 
-//     beforeEach('Create connection', function () {
-//         connId = 'augm-search-conn' + Date.now().toString()
-//         databaseConnector.createConnection('myHost', '9201', connId)
-//     })
+    beforeEach('Create connection', function () {
+        connId = 'augm-search-conn' + Date.now().toString()
+        databaseConnector.createConnection('myHost', '9201', connId)
+    })
 
-//     afterEach('Delete connection', function () {
-//         databaseConnector.cleanUp()
-//     })
+    afterEach('Delete connection', function () {
+        databaseConnector.cleanUp()
+    })
 
-//     it('edit a new connection with error', function () {
-//         databaseConnector.goTo()
-//         databaseConnector
-//             .clickOnEditConnection(connId)
-//             .editElasticSearchConnection('mynewhost', '9202')
-//             .verifyFailedConnectionMessage()
-//         databaseConnector.verifyConnectionExists('mynewhost', '9202', connId)
-//     })
+    it('edit a new connection with error', function () {
+        databaseConnector.goTo()
+        databaseConnector
+            .clickOnEditConnection(connId)
+            .editElasticSearchConnection('mynewhost', '9202')
+            .verifyFailedConnectionMessage()
+        databaseConnector.verifyConnectionExists('mynewhost', '9202', connId)
+    })
 
-//     it('edit a new connection successfully', function () {
-//         databaseConnector.goTo()
-//         databaseConnector
-//             .clickOnEditConnection(connId)
-//             .editElasticSearchConnection('elasticsearch', '9200')
-//             .verifyValidConnectionMessage()
-//         databaseConnector.verifyConnectionExists('elasticsearch', '9200', connId)
-//     })
+    it('edit a new connection successfully', function () {
+        databaseConnector.goTo()
+        databaseConnector
+            .clickOnEditConnection(connId)
+            .editElasticSearchConnection('elasticsearch', '9200')
+            .verifyValidConnectionMessage()
+        databaseConnector.verifyConnectionExists('elasticsearch', '9200', connId)
+    })
 
-//     it('check id cannot be edited', function () {
-//         databaseConnector.goTo()
-//         databaseConnector.clickOnEditConnection(connId).verifyElementIsreadOnly().cancelEditConnection()
-//     })
-// })
+    it('check id cannot be edited', function () {
+        databaseConnector.goTo()
+        databaseConnector.clickOnEditConnection(connId).verifyElementIsreadOnly().cancelEditConnection()
+    })
+})
