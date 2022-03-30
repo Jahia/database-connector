f0cb5d4d88abe55a56ae49efc262e8dded03b256
Thomas Draier <tdraier@jahia.com>
2022-03-30 18:51:49 +0200

    BACKLOG-16745: Update test dependencies

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
 const codeCoverage = require('@cypress/code-coverage/task');
 const installLogsPrinter = require('cypress-terminal-report/src/installLogsPrinter');
 const taskApolloNodeQuery = require('./taskApolloNodeQuery')
@@ -13,7 +12,6 @@ module.exports = (on, config) => {
     //https://github.com/archfz/cypress-terminal-report
     // eslint-disable-next-line @typescript-eslint/no-var-requires    
     installLogsPrinter(on);
-    on("file:preprocessor", cypressTypeScriptPreprocessor);
     on('task', {
         apolloNodeQuery(params) {
             console.log(config.env)
